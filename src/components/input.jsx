8b49a343feef8a5f015b821ac825a06868b68100
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-03-11 16:55:10 +0100

    Add extra class to email input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,12 +2,17 @@ function Input(props) {
 
     const {name, onChangeFunc, placeholder, type, label, value, errorMessage} = props
 
+    // For the email input add an extra class to the icon and error-message such that we display the error elements only on the email input when there is an input error
+    const extraClassNameIcon = name === "email" ? "email-error-icon" : ""
+    const extraClassNameMessage = name === "email" ? "email-error-message" : ""
+    const extraClassNameBorder = name === "email" ? "email-error-input" : ""
+
     return (
         <div className={"input " + {name} + "-input"}>
             <input
                 onChange={onChangeFunc}
                 placeholder={placeholder}
-                className={name}
+                className={name + " " + `${extraClassNameBorder}`}
                 type={type}
                 name={name}
                 aria-label={label}
@@ -15,17 +20,16 @@ function Input(props) {
                 autoComplete="none"
             />
 
+            <p className={"error-message " + `${extraClassNameMessage}`}>
+                <i>{errorMessage}</i>
+            </p>
+
             <img
-                className="error-icon"
+                className={"error-icon " + `${extraClassNameIcon}`}
                 src="./images/icon-error.svg"
                 alt="Input Error Icon"
             />
-
-            <p className="error-message">
-                <i>{errorMessage}</i>
-            </p>
         </div>
     )
 }
 
-export default Input;
